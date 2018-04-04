0b7202ac3b994650e2e0e3ea5283806858c26a05
Kenny Code <kenny030217@gmail.com>
2018-04-04 17:28:59 -0300

    Spring Boot configured to use WebContainer (Tomcat 8.0).

---
+++ src/main/java/com/kennycode/hyperengagesmvc/App.java
@@ -0,0 +1,25 @@
+package com.kennycode.hyperengagesmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer {
+
+	private static Class<App> applicationClass = App.class;
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+}
